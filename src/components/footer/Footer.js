cf917ef1f5306ab49647c1c0b61444da8391f81b
ozkankomu <ozkankomu35@gmail.com>
2022-11-07 03:23:06 +0300

    finished

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,19 +8,19 @@ const Footer = () => {
   return (
     <footer className="m-0">
       <p>Ozkan Web Design, Copyright &copy; {new Date().getFullYear()}</p>
-      <div>
+      <div className="footer">
         <a href="https://www.linkedin.com/in/ozkankomu/" target="_blanked">
-          <LinkedInIcon fontSize="large" color="error" />
+          <LinkedInIcon fontSize="large" />
         </a>
         <a
           className="p-3"
           href="https://github.com/ozkankomu"
           target="_blanked"
         >
-          <GitHubIcon fontSize="large" color="error" />
+          <GitHubIcon fontSize="large" />
         </a>
         <a href="https://ozkankomu35@gmail.com" target="_blanked">
-          <MailIcon fontSize="large" color="error" />
+          <MailIcon fontSize="large" />
         </a>
       </div>
     </footer>
